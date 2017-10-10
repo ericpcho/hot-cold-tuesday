64d0f89374e95b96c215b3feb0d8eb8d7c040a47
ericpcho <ericpcho@gmail.com>
2017-10-10 09:11:23 -0700

    setup our index.js with our provider tags and imported provider from react-redux

--- src/index.js
+++ src/index.js
@@ -9,22 +9,21 @@ import Game from './components/game';
 
 import store from './store';
 import * as actions from './actions'; // import our actions
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-    <Game />,
+    <Provider store={store}>
+    <Game />
+    </Provider>,
     document.getElementById('root')
 );
 
-console.log(store.getState());
 
-store.dispatch(actions.guess(10))
 
-console.log(store.getState());
-
-store.dispatch(actions.newGame())
-
-console.log(store.getState());
-
-store.dispatch(actions.toggleInfoModal())
-
-console.log(store.getState())
+// console.log(store.getState());
+// store.dispatch(actions.guess(10))
+// console.log(store.getState());
+// store.dispatch(actions.newGame())
+// console.log(store.getState());
+// store.dispatch(actions.toggleInfoModal())
+// console.log(store.getState())
